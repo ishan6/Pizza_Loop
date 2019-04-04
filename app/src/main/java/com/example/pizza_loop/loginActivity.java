8eb4fe39bfe30dfa785a9c10c424c151cf9e9b44
ishan6 <ishanmenaka6@gmail.com>
2019-04-04 21:47:51 +0530

    Login3

--- app/src/main/java/com/example/pizza_loop/loginActivity.java
+++ app/src/main/java/com/example/pizza_loop/loginActivity.java
@@ -1,13 +1,32 @@
 package com.example.pizza_loop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = findViewById(R.id.sign_up);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openlogin();
+            }
+        });
+
+    }
+
+    public void openlogin(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
